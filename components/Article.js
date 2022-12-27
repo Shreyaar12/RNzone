3b41a5eef0cf672a5c80503d30e8f392a6fd2d98
Shreyaar12 <shreyarajpal6@gmail.com>
2022-12-27 20:15:34 +0530

    card css completed  in article commit

--- components/Article.js
+++ components/Article.js
@@ -1,109 +1,103 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Alert, Button,Image, Text, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Alert,
+  Button,
+  Image,
+  Text,
+  SafeAreaView,
+} from "react-native";
 
-
-const Article = ()=> {
+const Article = () => {
   return (
     <SafeAreaView style={styles.container}>
-        {/* image */}
-        <Image source={{
-            url:"../images/thisisengineering.jpg"
+      {/* image */}
+      <Image
+        source={{
+          url: "https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         }}
-        style={styles.image}/>
-        {/* title */}
-<View style={{padding: 20}}>
-  <Text style={styles.title}> News
-    Article
-  </Text>
-  {/* description */}
-  <Text style={styles.description}>
-  Lorem ipsum dolor sit amet, consectetur
-  Lorem ipsum dolor sit amet, consectetur
-  Lorem ipsum dolor sit amet, consectetur
-  Lorem ipsum dolor sit amet, consectetur
-  </Text>
-  {/* data */}
-  <View style={styles.data}>
-    <Text style={styles.heading}>
-        by: <Text style={styles.author}>
-            Shreya Rajpal
+        style={styles.image}
+      />
+      {/* title */}
+      <View style={{ padding: 20 }}>
+        <Text style={styles.title}> News Article</Text>
+        {/* description */}
+        <Text style={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet,
+          consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor
+          sit amet, consectetur
         </Text>
-    </Text>
-    <Text style={styles.date}> Aug 9th 22</Text>
-  </View>
+        {/* data */}
+        <View style={styles.data}>
+          <Text style={styles.heading}>
+            by: <Text style={styles.author}>Shreya Rajpal</Text>
+          </Text>
+          <Text style={styles.date}> Aug 9th 22</Text>
+        </View>
 
-{/* Source */}
-<View style={{marginTop:10}}>
-<Text style={styles.source}> Hindustan Times</Text>
-</View>
-</View>
+        {/* Source */}
+        <View style={{ marginTop: 10 }}>
+          <Text style={styles.source}> Hindustan Times</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
-      </SafeAreaView>)
-      
-
-
-}
+    </SafeAreaView>
+  );
+};
 
 export default Article;
 
-
 const styles = StyleSheet.create({
   container: {
-    width:"90%",
-    alignSelf:"center",
-    borderRadius:20,
-    shadowOpacity:0.5,
-    shadowColor:"#000",
-    shadowOffset:{
-        height:5,
-        width:5
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 20,
+    shadowOpacity: 0.5,
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 5,
+      width: 5,
     },
 
-
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  image:{
-    height:200,
+  image: {
+    height: 200,
     width: "100%",
     borderTopLeftRadius: 40,
-    borderTopRightRadius: 40
+    borderTopRightRadius: 40,
   },
-  title:{
-    fontSize:18,
-    fontWeight:"600",
-    marginTop:10
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 10,
   },
-  description:{
-    fontSize:16,
-    fontWeight:"400",
-    marginTop:10
+  description: {
+    fontSize: 16,
+    fontWeight: "400",
+    marginTop: 10,
   },
-  data:{
-   flexDirection: "row",
-   justifyContent:"space-between",
-   marginTop:10
+  data: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
   },
-  date:{ 
-   fontWeight:"bold",
-   color:"red",
-   fontSize: 15
+  date: {
+    fontWeight: "bold",
+    color: "red",
+    fontSize: 15,
   },
-  heading:{
-  
+  heading: {},
+  author: {
+    fontWeight: "bold",
   },
-  author:{
-    fontWeight:"bold",
-
+  source: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 15,
   },
-  source:{
-
-color:"red",
-fontWeight:"bold",
-fontSize: 15
-
-  }
-
 });
