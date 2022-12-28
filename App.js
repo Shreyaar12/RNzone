e21d996cfa360a2364096595a865c6378c9accdd
Shreyaar12 <shreyarajpal6@gmail.com>
2022-12-28 11:43:43 +0530

    bar icons  added

--- App.js
+++ App.js
@@ -2,18 +2,34 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Alert, Button, Text, SafeAreaView } from "react-native";
 import Article from "./components/Article";
 import HomeScreen from "./screens/HomeScreen";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SearchScreen from "./components/SearchScreen";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-    </Tab.Navigator>
-  </NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "purple",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+          }}
+        />
+        <Tab.Screen name="Search" component={SearchScreen}
+        options={{
+          tabBarIcon:()=><FontAwesome name="search" size={24} color="black" />
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
