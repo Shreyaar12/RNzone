13dc88b7942eb9d56288d9f3ca9cba5968289951
Shreyaar12 <shreyarajpal6@gmail.com>
2022-12-28 11:01:44 +0530

    Search screen added

---
+++ components/SearchScreen.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View, Text } from "react-native";
+import HomeScreen from "../screens/HomeScreen";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen </Text>{" "}
+    </View>
+  );
+};
